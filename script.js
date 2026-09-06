af8cb1c2a7a5b2f2be30dca4f79e9d921cd590b9
AbuYahya <saidevv.ai@gmail.com>
2026-09-06 08:07:55 +0500

    nauchilsya podkluchat ssilky k JavaScript

---
+++ script.js
@@ -0,0 +1,6 @@
+var knopka = document.querySelector('.burger');
+var menyu = document.querySelector('.ssylki');
+
+knopka.addEventListener('click', function () {
+                      menyu.classList.toggle('otkryto');
+              });
