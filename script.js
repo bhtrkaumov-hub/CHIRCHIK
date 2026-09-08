751c1504ee3b99de9406c2eddeb907bf5c98e8ae
AbuYahya <saidevv.ai@gmail.com>
2026-09-08 19:33:44 +0500

    menyu menyayetsa na krestik kogda otkrivayetsa

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ var menyu = document.querySelector('.ssylki');
 
 knopka.addEventListener('click', function () {
                       menyu.classList.toggle('otkryto');
-              });
+   
+
+      if (menyu.classList.contains('otkryto')) {
+              knopka.textContent = '✕';
+      } else {
+              knopka.textContent = '☰';
+      }
+});
